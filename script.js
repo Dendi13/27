1a6de6a04a98e2ba997f1068c52272cabeeb7eab
Dendi13 <98680907+Dendi13@users.noreply.github.com>
2023-03-03 18:10:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 let parent = document.querySelector('.nav__list');
 let element = document.querySelector('.nav__item');
 let link = document.querySelector('.nav__link');
-let btn = document.querySelector('.btn');
-let form = document.querySelector('.form');
+let myBtn = document.querySelector('#myBtn');
+let Myform = document.querySelector('.form');
+let orderNow = document.querySelector('#orderNow');
+let btnOkn = document.querySelector('#btnOkn');
+let popup = document.querySelector('.popup');
+let form__close = document.querySelector('.form__close');
+let form__input = document.querySelector('.form__input');
+let popup__msg = document.querySelector('.popup__msg');
+let form__row = document.querySelector('.form__row')
 
-parent.addEventListener('click', function (event) {
+
+
+parent.addEventListener('click', function(event) {
     let currentElement = event.target;
-    for (let el of element) {
-        classList.parentElement().remove('active');
-        currentElement.classList.add('active');
+    console.log(event.target)
+    for( let el of element) {
+        el.parentElement.classList.remove('active');
+        currentElement.parentElement.classList.add('active');
     }
 })
 
-btn.addEventListener('click', function () {
-    currentElement.classList.add('active');
-
+orderNow.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    Myform.classList.add('active')
+    btnOkn.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        Myform.classList.remove('active');
+        popup.classList.add('active');
+        popup__msg.textContent = `Уважаемый ${form__input.value}, спасибо за заказ: ${form__row.value}`;
+    })
+    form__close.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        Myform.classList.remove('active');
+        popup.classList.remove('active');
+    })
 })
